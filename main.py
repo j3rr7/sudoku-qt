1286b9d77090bee756e51bb834171b79aed594c2
Jeremy <29926331+j3rr7@users.noreply.github.com>
2023-12-02 00:01:57 +0700

    Update
    * Add more UI's
    * Update sudoku utility
    * add ToDo's

--- main.py
+++ main.py
@@ -1,18 +1,105 @@
 # -*- coding: utf-8 -*-
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import QTimer, QTime, QRegExp
+from PyQt5.QtGui import QRegExpValidator
 import sys
 from sudoku import Sudoku
+import numpy as np
+
 
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        self.setFixedSize(650, 450)
+        self.about_window = None
+        self.score_window = None
         uic.loadUi('MainWindow.ui', self)
         self.show()
 
+        # region Setup Buttons Matrix
+        pattern = QRegExp("[1-9]")
+        rx = QRegExpValidator(pattern)
+        self.button_matrix = [[None for _ in range(9)] for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                le_name = f"le_{i}_{j}"
+                line_edit = self.findChild(QtWidgets.QLineEdit, le_name)
+                line_edit.setMaxLength(1)
+                line_edit.setValidator(rx)
+                self.button_matrix[i][j] = line_edit
+        # endregion
+
+        # region Fields
+        self.game_running = False
+
+        self.timer = None
+        self.timer_counter = 0
+
+        # endregion
+
+        self.setup_timer()
+        self.setup_buttons()
+        # self.setup_game()
+
+    def setup_timer(self):
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_timer_label)
+        self.timer_counter = QTime(0, 0, 0)
+
+    def setup_buttons(self):
+        self.btn_start.clicked.connect(self.toggle_start_stop)
+        self.btn_reset.clicked.connect(self.reset_game)
+        self.btn_about.clicked.connect(self.open_about_window)
+        self.btn_highscore.clicked.connect(self.open_score_window)
+
+    def setup_game(self):
         sudoku = Sudoku().mask_numbers(0.5)
         print(sudoku)
+
+    # region Timer Functions
+    def update_timer_label(self):
+        self.timer_counter = self.timer_counter.addSecs(1)
+        self.lbl_timer.setText(self.timer_counter.toString("HH:mm:ss"))
+
+    def toggle_start_stop(self):
+        if self.game_running:
+            self.timer.stop()
+            self.btn_start.setText("Start")
+            self.game_running = False
+        else:
+            self.reset_game()
+            self.timer.start(1000)
+            self.btn_start.setText("Stop")
+            self.game_running = True
+
+    def reset_game(self):
+        self.timer.stop()
+        self.timer_counter = QTime(0, 0)
+        self.btn_start.setText("Start")
+        self.lbl_timer.setText("00:00:00")
+        self.game_running = False
+
+    def open_about_window(self):
+        self.about_window = AboutWindow()
+        self.about_window.show()
+
+    def open_score_window(self):
+        self.score_window = ScoreWindow()
+        self.score_window.show()
+    # endregion
+
+
+class AboutWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(AboutWindow, self).__init__()
+        uic.loadUi('AboutWindow.ui', self)
+
+
+class ScoreWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(ScoreWindow, self).__init__()
+        uic.loadUi('ScoreWindow.ui', self)
 
 
 if __name__ == '__main__':
--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from random import sample
 
 
 # def generate_board(self):
@@ -15,64 +16,92 @@ import numpy as np
 #                 return False
 #     return True
 
-class Sudoku:
-    def __init__(self):
-        self.board = np.zeros((9, 9), dtype=int)
-        self._rows = np.arange(9)
-        self._cols = np.arange(9)
+# class Sudoku:
+#     def __init__(self):
+#         self.board = np.zeros((9, 9), dtype=int)
+#         self._rows = np.arange(9)
+#         self._cols = np.arange(9)
+#
+#         self.generate_board()
+#
+#     def generate_board(self):
+#         for i in self._rows:
+#             for j in self._cols:
+#                 if self.board[i][j] == 0:
+#                     valid_nums = [num for num in range(1, 10) if self.is_valid(i, j, num)]
+#                     if valid_nums:
+#                         for num in valid_nums:
+#                             self.board[i][j] = num
+#                             if self.generate_board():
+#                                 return True
+#                             self.board[i][j] = 0
+#                     return False
+#         return True
+#
+#     def regenerate_board(self):
+#         self.board = np.zeros((9, 9), dtype=int)
+#         self.generate_board()
+#
+#     def is_valid(self, row, col, num):
+#         if np.any(self.board[row] == num):
+#             return False
+#         if np.any(self.board[:, col] == num):
+#             return False
+#         start_row = row - row % 3
+#         start_col = col - col % 3
+#         if np.any(self.board[start_row:start_row + 3, start_col:start_col + 3] == num):
+#             return False
+#         return True
+#
+#     def mask_numbers(self, difficulty):
+#         num_to_mask = int(91 * difficulty)
+#         num_to_mask = min(num_to_mask, 81)  # Ensure num_to_mask does not exceed 81
+#         flattened_board = self.board.flatten()
+#         masked_indices = np.random.choice(np.arange(81), size=num_to_mask, replace=False)
+#         flattened_board[masked_indices] = 0
+#         # self.board = flattened_board.reshape(9, 9)
+#         return flattened_board.reshape(9, 9)
+#
+#     def print_board(self):
+#         print(self.board)
+#
 
-        self.generate_board()
+
+class Sudoku:
+    def __init__(self, base=3):
+        self.board = None
+        self.base = base
+        self.side = base * base
+
+    def pattern(self, r, c):
+        return (self.base * (r % self.base) + r // self.base + c) % self.side
+
+    def shuffle(self, s):
+        return sample(s, len(s))
 
     def generate_board(self):
-        for i in self._rows:
-            for j in self._cols:
-                if self.board[i][j] == 0:
-                    valid_nums = [num for num in range(1, 10) if self.is_valid(i, j, num)]
-                    if valid_nums:
-                        for num in valid_nums:
-                            self.board[i][j] = num
-                            if self.generate_board():
-                                return True
-                            self.board[i][j] = 0
-                    return False
-        return True
+        rBase = range(self.base)
+        rows = [g * self.base + r for g in self.shuffle(rBase) for r in self.shuffle(rBase)]
+        cols = [g * self.base + c for g in self.shuffle(rBase) for c in self.shuffle(rBase)]
+        nums = self.shuffle(range(1, self.base * self.base + 1))
 
-    def regenerate_board(self):
-        self.board = np.zeros((9, 9), dtype=int)
-        self.generate_board()
+        # produce board using randomized baseline pattern
+        board = [[nums[self.pattern(r, c)] for c in cols] for r in rows]
 
-    def is_valid(self, row, col, num):
-        if np.any(self.board[row] == num):
-            return False
-        if np.any(self.board[:, col] == num):
-            return False
-        start_row = row - row % 3
-        start_col = col - col % 3
-        if np.any(self.board[start_row:start_row + 3, start_col:start_col + 3] == num):
-            return False
-        return True
+        squares = self.side * self.side
+        empties = squares * 3 // 4
+        for _ in sample(range(squares), empties):
+            r, c = divmod(_, self.side)
+            board[r][c] = 0
 
-    def mask_numbers(self, difficulty):
-        num_to_mask = int(91 * difficulty)
-        num_to_mask = min(num_to_mask, 81)  # Ensure num_to_mask does not exceed 81
-        flattened_board = self.board.flatten()
-        masked_indices = np.random.choice(np.arange(81), size=num_to_mask, replace=False)
-        flattened_board[masked_indices] = 0
-        # self.board = flattened_board.reshape(9, 9)
-        return flattened_board.reshape(9, 9)
+        self.board = np.array(board)
 
     def print_board(self):
         print(self.board)
 
 
 if __name__ == '__main__':
-    sudoku = Sudoku()
-    sudoku.print_board()
-    new_board = sudoku.mask_numbers(0.5)
-    print(new_board)
-
+    sudoku = Sudoku(base=3)
     sudoku.generate_board()
-    sudoku.print_board()
 
-
-
+    print(sudoku.board)
